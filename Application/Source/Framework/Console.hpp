c0cde10f5bf896f70b607a5816373d7d0da152c7
slud <slud@o2.pl>
2012-07-06 22:30:51 +0200

    some project's lib paths management

---
+++ Application/Source/Framework/Console.hpp
@@ -0,0 +1,88 @@
+/**
+ *  Class header template version 3 - increases
+ *  each time this template changes.
+ * 
+ *  @file name
+ *  Brief description.
+ *  Details description.
+ */
+
+// LICENSE
+
+#ifndef AS_CONSOLE_INCLUDE
+    #define AS_CONSOLE_INCLUDE
+
+// INCLUDES
+
+// 1. Corresponded header file
+// 2. Necessary project headers
+// 3. 3rd party libraries headers
+// 4. Standard libraries headers
+#include <stack>
+// 5. System headers
+
+// MACROS
+
+/**
+ *  CExample
+ *  Class description.
+ *  Mind definitions:
+ *  1. Constructor.
+ *  2. Copy constructor.
+ *  3. Virtual destructor.
+ *  4. Assignment operator.
+ */
+template<typename T, typename Container = std::stack<T> >
+class CConsole
+{
+public:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+	void Transmit(const char* text);
+
+// DATA
+
+protected:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+// DATA
+
+private:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+// DATA
+
+};
+
+// INLINE METHODS
+
+// TEMPLATE DEFINITIONS
+
+// REFERENCES
+
+#endif // AS_CONSOLE_INCLUDE
+
+
+
+
---
+++ Application/Source/Tests/Framework/ConsoleTest.cpp
@@ -0,0 +1,21 @@
+#include "ConsoleTest.hpp"
+#include "Framework/Console.hpp"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( CConsoleTest );
+
+
+void CConsoleTest::setUp()
+{
+}
+
+
+void CConsoleTest::tearDown()
+{
+}
+
+
+void CConsoleTest::test1()
+{
+
+}
