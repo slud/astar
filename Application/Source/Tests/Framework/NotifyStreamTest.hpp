1f521a9a255548340f3acc81fa0ef0f8f0b4e8cf
slud <slud@o2.pl>
2012-08-07 11:37:50 +0200

    hmm... confused

---
+++ Application/Source/Tests/Framework/NotifyStreamTest.hpp
@@ -0,0 +1,94 @@
+/**
+ *  Class header template version 3 - increases
+ *  each time this template changes.
+ * 
+ *  @file name
+ *  Brief description.
+ *  Details description.
+ */
+
+// LICENSE
+
+#ifndef AS_NOTIFY_STREAM_TEST_INCLUDE
+    #define AS_NOTIFY_STREAM_TEST_INCLUDE
+
+// INCLUDES
+
+// 1. Corresponded header file
+// 2. Necessary project headers
+// 3. 3rd party libraries headers
+#include <cppunit/extensions/HelperMacros.h>
+// 4. Standard libraries headers
+// 5. System headers
+
+// MACROS
+
+/**
+ *  CExample
+ *  Class description.
+ *  Mind definitions:
+ *  1. Constructor.
+ *  2. Copy constructor.
+ *  3. Virtual destructor.
+ *  4. Assignment operator.
+ */
+class CNotifyStreamTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( CNotifyStreamTest );
+		CPPUNIT_TEST( Test1 );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+	void setUp();
+	void tearDown();
+
+	void Test1();
+
+// DATA
+
+protected:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+// DATA
+
+private:
+
+// TYPES
+
+// LIFECYCLE
+
+// OPERATORS
+
+// METHODS
+
+// DATA
+
+};
+
+// INLINE METHODS
+
+// TEMPLATE DEFINITIONS
+
+// REFERENCES
+
+#endif // AS_NOTIFY_STREAM_TEST_INCLUDE
+
+
+
+
