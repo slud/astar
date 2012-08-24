500c23bd3a90fb18e6cb317b98e76c945001e68f
slud <slud@o2.pl>
2012-08-24 15:49:12 +0200

    sync

--- Application/Source/Renderer/OpenGLRenderer.cpp
+++ Application/Source/Renderer/OpenGLRenderer.cpp
@@ -11,9 +11,31 @@ extern AS::Settings::CSettingsSimple gSettings;
 
 static stb_fontchar fontdata[STB_SOMEFONT_NUM_CHARS];
 static GLuint gTextureID = 0;
+static GLuint gCheckerID = 0;
+static GLuint gChecker[128*128];
 static void init(void)
 {
-	
+	for(int i=0; i<128*128; i++)
+	{
+		GLubyte* colors = (GLubyte*)&gChecker[ i ];
+       //If the 5th bit of the x and y offsets of the pixel do not match
+        if( i / 128 & 16 ^ i % 128 & 16 )
+        {
+            //Set pixel to white
+            colors[ 0 ] = 0xFF;
+            colors[ 1 ] = 0xFF;
+            colors[ 2 ] = 0xFF;
+            colors[ 3 ] = 0xFF;
+        }
+        else
+        {
+            //Set pixel to red
+            colors[ 0 ] = 0xFF;
+            colors[ 1 ] = 0x00;
+            colors[ 2 ] = 0x00;
+            colors[ 3 ] = 0xFF;
+        }
+	}
     // optionally replace both STB_SOMEFONT_BITMAP_HEIGHT with STB_SOMEFONT_BITMAP_HEIGHT_POW2
     static GLubyte fontpixels[STB_SOMEFONT_BITMAP_HEIGHT_POW2][STB_SOMEFONT_BITMAP_WIDTH];
 	//GLuint fontpixels1[STB_SOMEFONT_BITMAP_HEIGHT_POW2][STB_SOMEFONT_BITMAP_WIDTH] = {0};
@@ -21,9 +43,16 @@ static void init(void)
     //... create texture ...
 	glGenTextures(1, &gTextureID);
 	glBindTexture(GL_TEXTURE_2D, gTextureID );
-	glTexImage2D(GL_TEXTURE_2D, 0, 1, STB_SOMEFONT_BITMAP_WIDTH, STB_SOMEFONT_BITMAP_HEIGHT_POW2, 0, 1, GL_UNSIGNED_BYTE, fontpixels );
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, STB_SOMEFONT_BITMAP_WIDTH, STB_SOMEFONT_BITMAP_HEIGHT_POW2, 0, GL_RED, GL_UNSIGNED_BYTE, fontpixels );
+	glBindTexture(GL_TEXTURE_2D, NULL);
+
+	glGenTextures(1, &gCheckerID);
+	glBindTexture(GL_TEXTURE_2D, gCheckerID );
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, gChecker );
 	glBindTexture(GL_TEXTURE_2D, NULL);
     // for best results rendering 1:1 pixels texels, use nearest-neighbor sampling
     // if allowed to scale up, use bilerp
@@ -34,8 +63,8 @@ static void draw_string_integer(int x, int y, char *str) // draw with top-left p
     //... use texture ...
 	glBindTexture(GL_TEXTURE_2D, gTextureID);
     //... turn on alpha blending and gamma-correct alpha blending ...
-	glEnable(GL_ALPHA_TEST);
-	glEnable(GL_BLEND);
+	//glEnable(GL_ALPHA_TEST);
+	//glEnable(GL_BLEND);
     glBegin(GL_QUADS);
     while (*str) {
         int char_codepoint = *str++;
@@ -51,6 +80,25 @@ static void draw_string_integer(int x, int y, char *str) // draw with top-left p
 		glVertex2i(x + cd->x0, y + cd->y1);
         // if bilerping, in D3D9 you'll need a half-pixel offset here for 1:1 to behave correct
         x += cd->advance_int;
+    }
+    glEnd();
+}
+
+static void draw_string_float(float x, float y, char *str) // draw with top-left point x,y
+{
+    //... use texture ...
+    //... turn on alpha blending and gamma-correct alpha blending ...
+	glBindTexture(GL_TEXTURE_2D, gTextureID);
+    glBegin(GL_QUADS);
+    while (*str) {
+        int char_codepoint = *str++;
+        stb_fontchar *cd = &fontdata[char_codepoint - STB_SOMEFONT_FIRST_CHAR];
+        glTexCoord2f(cd->s0f, cd->t0f); glVertex2f(x + cd->x0f, y + cd->y0f);
+        glTexCoord2f(cd->s1f, cd->t0f); glVertex2f(x + cd->x1f, y + cd->y0f);
+        glTexCoord2f(cd->s1f, cd->t1f); glVertex2f(x + cd->x1f, y + cd->y1f);
+        glTexCoord2f(cd->s0f, cd->t1f); glVertex2f(x + cd->x0f, y + cd->y1f);
+        // if bilerping, in D3D9 you'll need a half-pixel offset here for 1:1 to behave correct
+        x += cd->advance;
     }
     glEnd();
 }
@@ -103,14 +151,24 @@ static void DrawTester(float xs, float ys, float zs)
 	float xo = xs;
 	float yo = ys;
 	float zo = zs;
+	int id = gTextureID;
+	//int id = gCheckerID;
+	glBindTexture(GL_TEXTURE_2D, id);
 	glBegin( GL_QUADS );
 		glColor3f(1.0f, 0.0f, 0.0f);
+		glTexCoord2f(0.0f,1.0f);
 		glVertex3f(-1.0f+xo, -1.0f+yo, 1.0f+zo);
-		glColor3f(0.0f, 1.0f, 0.0f);
+
+		//glColor3f(0.0f, 1.0f, 0.0f);
+		glTexCoord2f(1.0f,1.0f);
 		glVertex3f(1.0f+xo, -1.0f+yo, 1.0f+zo);
-		glColor3f(0.0f, 0.0f, 1.0f);
+
+		//glColor3f(0.0f, 0.0f, 1.0f);
+		glTexCoord2f(1.0f,0.0f);
 		glVertex3f(1.0f+xo, 1.0f+yo, 1.0f+zo);
-		glColor3f(0.0f, 0.0f, 0.0f);
+
+		//glColor3f(0.0f, 0.0f, 0.0f);
+		glTexCoord2f(0.0f,0.0f);
 		glVertex3f(-1.0f+xo, 1.0f+yo, 1.0f+zo);
 	glEnd();
 }
@@ -123,12 +181,15 @@ void AS::Rendering::COpenGLRenderer::Render()
 	ticks = SDL_GetTicks();
 
 	glClear( GL_COLOR_BUFFER_BIT );
+	//glLoadIdentity();
+	//DrawTester(0.0,0.0,-6.0);
 
-	DrawTester(0.0,0.0,-6.0);
-
-	glLoadIdentity();
-	glTranslatef(0.0f,0.0f,-3.0f);
-	draw_string_integer(0,0,"hello world");
+	
+	//glTranslatef(0.0f,0.0f,-6.0f);
+	//draw_string_integer(0,0,"hello world");
+	//glScalef(0.05f,0.05f,0.05f);
+	//glRotatef(180.0f, 1.0f, 0.0f, 0.0f);
+	//draw_string_float(0.0f,0.0f,"bebebe kokoko");
 /*
 	glMatrixMode( GL_MODELVIEW );
 	glLoadIdentity();
